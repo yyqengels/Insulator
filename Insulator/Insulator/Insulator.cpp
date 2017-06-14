d155fa1b04e1e9f28e3b5f2a9ab4cd6456fa1e7b
yyqengels <yyqengels@gmail.com>
2017-06-13 23:06:28 -0500

    Bugs fixed

--- Insulator/Insulator/Insulator.cpp
+++ Insulator/Insulator/Insulator.cpp
@@ -8,16 +8,41 @@
 #include <cstdio>
 #include <algorithm>
 
+#include "ACJ.h"
+#include "Codes.h"
+#include "Complex.h"
+#include "roundoff.h"
+#include "SL2ACJ.h"
+
+
+
 using namespace std;
 
 
-
+vector<string> root;
+char divid[2] = { '0','1' };
+void rootGen(string res = "", int k = 0)
+{
+	if (k != 0)	root.push_back(res);
+	if (k == 6)
+	{
+		return;
+	}
+	for (int i = 0; i < 2; i++)
+	{
+		res += divid[i];
+		rootGen(res, k + 1);
+		res.erase(res.end() - 1);
+	}
+}
+/////////////////////////////////////////
 int K;
 vector<char> killWord({ 'f', 'w', 'F', 'W' });
 vector<char> intCon({ 'L','O','2' });
 int N = 4;
-vector<pair<int, string>> conList; // the first element of conList is the ranking of the second element
-								   // funGen is used to generate all the killerwords with length less than or equal to K
+// the first element of conList is the ranking of the second element
+vector<pair<int, string>> conList; 
+// funGen is used to generate all the killerwords with length less than or equal to K
 void funGen(string res, int k)
 {
 	if (k != 0)	conList.push_back(make_pair(0, res));
@@ -58,9 +83,7 @@ void funGen(string res, int k)
 			}
 			res.erase(res.end() - 1);
 		}
-
 	}
-
 }
 ////////////////////////////////////////////////////
 
@@ -69,26 +92,38 @@ void appendFun()
 
 }
 
-
 // check if where is one of the 11 exceptional sub-boxes
-bool checkExcept(char*where, string exceptBox[11])
+bool checkExcept(char*where, vector<string> exceptBox)
 {
-	bool check[11];
-	for (int i = 0; i < 11; i++)
+	int i = 0;
+	while (i < 11)
 	{
-		check[i] = (where == *(exceptBox + i));
+		if (where == exceptBox[i])
+		{
+			return true;
+			break;
+		}
+		i++;
 	}
-	return any_of(begin(check), end(check), [](bool i) {return i == true; });
+	return false;
 }
 
-// check if one of the candidate conditions can kill where
-// and sort the candidate conditions based on their ``usefulness"
+ //check if one of the candidate conditions can kill where
+ //and sort the candidate conditions based on their ``usefulness"
 bool checkInequal(char*where, int depth)
 {
 	int i = 0;
-	while (i < conList.size())
+	while (i < int(conList.size()))
 	{
-		if (inequalityHolds(conList[i].second, where, depth))
+		string temp = conList[i].second;
+		if (i >= 6)
+		{// skip the first six conditions: trival conditions
+			// add left and right paranthesis to conList[i].second
+			temp.insert(temp.begin() + 1, '(');
+			temp.insert(temp.end(), ')');
+		}
+		const char *code = temp.c_str();
+		if (inequalityHolds(code , where, depth))
 		{
 			(conList[i].first)++;
 			// sort the condition list
@@ -102,17 +137,15 @@ bool checkInequal(char*where, int depth)
 	return false;
 }
 
+int maxDepth = 50;
 
+vector<string> failBox;
 
-
-int maxDepth = 10;
-
-
-
-void subBox(char*where, int depth, string exceptBox[11])
+void subBox(char*where, int depth, vector<string> exceptBox)
 {
 	if (depth == maxDepth)
 	{// check if depth is too deep
+		failBox.push_back(where);
 		fprintf(stderr, "verify: fatal error at %s\n", where);
 		exit(1);
 	}
@@ -129,8 +162,15 @@ void subBox(char*where, int depth, string exceptBox[11])
 
 
 		  // subdivide the current box
-		subBox(where + '0', depth + 1, exceptBox);
-		subBox(where + '1', depth + 1, exceptBox);
+		// left child
+		string temp = string(where);
+		temp += '0';
+		where = &temp[0];
+		subBox(where, depth + 1, exceptBox);
+		// right child
+		temp[temp.size() - 1] = '1';
+		where = &temp[0];
+		subBox(where, depth + 1, exceptBox);
 	}
 }
 
@@ -138,7 +178,7 @@ void subBox(char*where, int depth, string exceptBox[11])
 int main()
 {
 	// codes for all 11 exceptional sub-boxes
-	string exceptBox[11] = {
+	vector<string> exceptBox({
 		"001000110111110001 101001010101011001 011011010111101101 100001101101000111 010001110101100101 1101110111110100",
 		"001001110110110000 101000010100011000 011010010110101100 100000101100000110 010000110100100100 1101100111100100",
 		"001000110001110110 011101000110111110 100010110000100011 101101001101001000 110101011000000100 000",
@@ -149,12 +189,13 @@ int main()
 		"001000110001110111 001111000101111111 101111100111001111 000001111011110111 1",
 		"001001110000110110 001110000100111110 101110100110001110 000000111010110110 1",
 		"111000000001000111 111111110101001111 011111010111111111 110001001011000111 0",
-		"111001000000000110 111110110100001110 011110010110111110 110000001010000110 0" };
+		"111001000000000110 111110110100001110 011110010110111110 110000001010000110 0" });
 	// get rid of spaces in the strings
 	for (int i = 0; i < 11; i++)
 	{
 		exceptBox[i].erase(remove_if(exceptBox[i].begin(), exceptBox[i].end(), isspace), exceptBox[i].end());
 	}
+	
 	/////////////////////////
 	// initialize the conList
 	conList.push_back(make_pair(0, "s"));
@@ -163,42 +204,18 @@ int main()
 	conList.push_back(make_pair(0, "f"));
 	conList.push_back(make_pair(0, "w"));
 	conList.push_back(make_pair(0, "W"));
-	K = 2;
+	K = 5;
 	funGen("", 0);
-	//cout << (conList[0].second[0] == 's') << endl;
+	rootGen();
 
-
-	cout << conList.size() << endl;
-	for (int i = 0; i < conList.size(); i++)
+	for (int i = 0; i < int(root.size()); i++)
 	{
-		cout << conList[i].first << endl;
-		cout << conList[i].second << endl;
-
+		if (root[i][3] == 1)
+		{
+			continue;
+		}
+		subBox(&root[i][0], root[i].size(), exceptBox);
 	}
-	conList[6].first += 5;
-	conList[7].first += 10;
-	conList[8].first += 7;
-	for (int i = 0; i < conList.size(); i++)
-	{
-		cout << conList[i].first << endl;
-		cout << conList[i].second << endl;
-
-	}
-	cout << "Start sorting" << endl;
-	sort(conList.rbegin(), conList.rend() - 6);
-	for (int i = 0; i < conList.size(); i++)
-	{
-		cout << conList[i].first << endl;
-		cout << conList[i].second << endl;
-
-	}
-	for (int i = 0; i < 11; i++)
-	{
-		cout << exceptBox[i] << endl;
-	}
-	///////////////////////////
-	string aaa[2] = { "ab","ajfdlksajlkdsfjlasjf" };
-	cout << *(aaa + 1) << endl;
 
 	return 0;
 }
