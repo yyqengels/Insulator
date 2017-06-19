033932aed440ca9c14a789bd0a3cb70002da9a97
yyqengels <yyqengels@gmail.com>
2017-06-18 23:46:06 -0500

    new features added

--- Insulator/Insulator/Insulator.cpp
+++ Insulator/Insulator/Insulator.cpp
@@ -15,7 +15,6 @@
 #include "SL2ACJ.h"
 
 
-
 using namespace std;
 
 
@@ -39,13 +38,61 @@ void rootGen(string res = "", int k = 0)
 int K;
 vector<char> killWord({ 'f', 'w', 'F', 'W' });
 vector<char> intCon({ 'L','O','2' });
+vector<char> triCon({ 's','l', 'n','f','w','W' });
 int N = 4;
-// the first element of conList is the ranking of the second element
-vector<pair<int, string>> conList; 
-// funGen is used to generate all the killerwords with length less than or equal to K
-void funGen(string res, int k)
+
+// Define the structure of "killer" function
+// field words is the function, e.g. fffwwww
+// field numUsed is the number of times a certain function has been used to kill a sub-box
+// field wordLength is the length of the function, e.g. the wordLength of the function fffwwww is 7
+// field transLength is the length of the translation of the function
+struct killFun
 {
-	if (k != 0)	conList.push_back(make_pair(0, res));
+	string words;
+	int numUsed;
+	int wordLength;
+	ACJ transLength;
+	killFun(ACJ t, string w = "", int n = 0, int wl = 0) : words(w), numUsed(n), wordLength(wl), transLength(t) {}
+};
+
+// Define the function to compare two killFuns
+// first sort by the number of times these two functions used 
+// second sort by the length of the functions
+// finally sort by the length of the the translations
+
+bool compare_entry(const killFun & e1, const killFun & e2) {
+	if (e1.numUsed != e2.numUsed)
+	{// sort in descending order
+		return (e1.numUsed > e2.numUsed);
+	}
+	else if (e1.wordLength != e2.wordLength)
+	{// sort in ascending order
+		return (e1.wordLength < e2.wordLength);
+	}// sort in ascending order
+	return (absUB(e1.transLength) < absUB(e2.transLength));
+}
+
+vector <killFun> conList;
+vector<killFun> newConList;
+// funGen is used to generate all the killerwords with length less than or equal to K
+void funGen(string res, int k, bool newCon = false)
+{
+	killFun tempKill;
+	tempKill.words = res;
+	tempKill.numUsed = 0;
+	tempKill.wordLength = size(res);
+	if (k != 0)
+	{
+		if (newCon)
+		{
+			newConList.push_back(tempKill);
+		}
+		else
+		{
+			conList.push_back(tempKill);
+		}
+	}
+
 	if (k == K)
 	{
 		return;
@@ -86,10 +133,17 @@ void funGen(string res, int k)
 	}
 }
 ////////////////////////////////////////////////////
-
-void appendFun()
+// initialCon is used to add the 6 trival conditions to the conList
+void initialCon()
 {
-
+	for (int i = 0; i < 6; i++)
+	{
+		killFun temp;
+		temp.words = triCon[i];
+		temp.numUsed = 0;
+		temp.wordLength = 1;
+		conList.push_back(temp);
+	}
 }
 
 // check if where is one of the 11 exceptional sub-boxes
@@ -110,12 +164,12 @@ bool checkExcept(char*where, vector<string> exceptBox)
 
  //check if one of the candidate conditions can kill where
  //and sort the candidate conditions based on their ``usefulness"
-bool checkInequal(char*where, int depth)
+bool checkInequal(vector <killFun> conList, char*where, int depth)
 {
 	int i = 0;
 	while (i < int(conList.size()))
 	{
-		string temp = conList[i].second;
+		string temp = conList[i].words;
 		if (i >= 6)
 		{// skip the first six conditions: trival conditions
 			// add left and right paranthesis to conList[i].second
@@ -125,10 +179,39 @@ bool checkInequal(char*where, int depth)
 		const char *code = temp.c_str();
 		if (inequalityHolds(code , where, depth))
 		{
-			(conList[i].first)++;
+			(conList[i].numUsed)++;// update the used times 
+			// find the translation length
+			double pos[6], size[6], scale[6];
+			for (int j = 0; j<6; j++) {
+				pos[j] = 0;
+				size[j] = 4;
+				scale[j] = pow(2, (5 - j) / 6.0);
+			}
+			for (int d = 0; d<depth; d++) {
+				size[d % 6] /= 2;
+				if (where[d] == '0') {
+					pos[d % 6] -= size[d % 6];
+				}
+				else if (where[d] == '1') {
+					pos[d % 6] += size[d % 6];
+				}
+				else {
+					assert(0);
+				}
+			}
+			for (int j = 0; j<6; j++) {
+				pos[j] *= scale[j];
+				size[j] = (1 + 2 * EPS)*(size[j] * scale[j] + HALFEPS*fabs(pos[j]));
+			}
+			ACJ along((XComplex(pos[0], pos[3])), XComplex(size[0], size[3]), 0, 0, 0);
+			ACJ ortho((XComplex(pos[1], pos[4])), 0, XComplex(size[1], size[4]), 0, 0);
+			ACJ whirle((XComplex(pos[2], pos[5])), 0, 0, XComplex(size[2], size[5]), 0);
+			SL2ACJ g(evaluateWord(code + 1, along, ortho, whirle));
+			conList[i].transLength = length(g);
+			////////////////////////////////////
 			// sort the condition list
 			// do not sort the first 6 conditions
-			sort(conList.rbegin(), conList.rend() - 6);
+			sort(conList.begin() + 6, conList.end(), compare_entry);
 			return true;
 			break;
 		}
@@ -137,40 +220,101 @@ bool checkInequal(char*where, int depth)
 	return false;
 }
 
-int maxDepth = 50;
+int maxDepth = 200;
 
-vector<string> failBox;
-
-void subBox(char*where, int depth, vector<string> exceptBox)
+struct node
 {
+	char *where;
+	int killIndex = 0;// 0 means not killed, 1 means killed 
+	node *parent = NULL;
+	node *left = NULL;
+	node *right = NULL;
+};
+
+
+bool dividBox(node *subBox, int depth, vector<string> exceptBox)//true means killed, false means not killed
+{
+	// First, check if depth is too deep
 	if (depth == maxDepth)
-	{// check if depth is too deep
-		failBox.push_back(where);
-		fprintf(stderr, "verify: fatal error at %s\n", where);
+	{
+		fprintf(stderr, "verify: fatal error at %s\n", subBox->where);
 		exit(1);
 	}
-	if (checkExcept(where, exceptBox))
+	// Then check if its parent is killed.
+	if (((subBox->parent)->killIndex) == 1)
 	{
-		cout << "Find exceptional sub-box" << where << endl;
+		return true;
 	}
-	else if (checkInequal(where, depth))
+	// If its parent is not killed 
+	// then first check if it is one of the 11 regions
+	if (checkExcept(subBox->where, exceptBox))
 	{
-		cout << "Killed" << where << endl;
+		cout << "Find exceptional sub-box" << (subBox->where) << endl;
+		return true;
+	}// if not, then check if it can be killed by one of the conditions
+	else if (checkInequal(conList,subBox->where, depth))
+	{
+		cout << "Killed" << subBox->where << endl;
+		return true;
 	}
-	else {// if every inequalityHold returns 0 then subdivide
-		  // and add new conditions with prefix being used condtions
+	else {// if every inequalityHold returns 0
+		  //then add a surfix to conditions that have been used 
+		vector<killFun> newConList;
+		for (int i = 6; i < conList.size(); i++)
+		{
+			if (conList[i].numUsed != 0)
+			{
+				funGen(conList[i].words, K-1,true);
+			}
+		}
 
-
-		  // subdivide the current box
+		// use new condition to check the current sub-box and its 6 parents
+		if (depth > 12)
+		{
+			vector<node> tempBox;
+			tempBox.push_back(subBox);
+			for (int i = 1; i <= 6; i++)
+			{
+				tempBox.push_back((tempBox[i - 1])->parent);
+			}
+			int i = 6;
+			while (i >= 0)
+			{
+				if (checkInequal(newConList,(tempBox[i])->where, depth - i))
+				{
+					(tempBox[i])->killIndex = 1;
+					//kill all the descendents
+					for (int j = 0; j < i; j++)
+					{
+						(tempBox[j])->killIndex = 1;
+					}
+					return true;
+					break;
+				}
+				i--;
+			}
+		}
+		for (int i = 0; i < newConList.size(); i++)
+		{
+			conList.push_back(newConList[i]);
+		}
+		sort(conList.begin() + 6, conList.end(), compare_entry);
+		// if none of them can be killed by the new conditions
+		// subdivide the current box
 		// left child
-		string temp = string(where);
+		subBox->left = new node;
+		string temp = string(subBox->where);
 		temp += '0';
-		where = &temp[0];
-		subBox(where, depth + 1, exceptBox);
+		subBox->left->where = &temp[0];
+		subBox->left->parent = subBox;
+		dividBox(subBox->left, depth + 1, exceptBox);
 		// right child
-		temp[temp.size() - 1] = '1';
-		where = &temp[0];
-		subBox(where, depth + 1, exceptBox);
+		subBox->right = new node;
+		string temp = string(subBox->where);
+		temp += '1';
+		subBox->right->where = &temp[0];
+		subBox->right->parent = subBox;
+		dividBox(subBox->right, depth + 1, exceptBox);
 	}
 }
 
@@ -198,23 +342,20 @@ int main()
 	
 	/////////////////////////
 	// initialize the conList
-	conList.push_back(make_pair(0, "s"));
-	conList.push_back(make_pair(0, "l"));
-	conList.push_back(make_pair(0, "n"));
-	conList.push_back(make_pair(0, "f"));
-	conList.push_back(make_pair(0, "w"));
-	conList.push_back(make_pair(0, "W"));
-	K = 5;
+	initialCon();
+	//
+	K = 10;
 	funGen("", 0);
 	rootGen();
-
 	for (int i = 0; i < int(root.size()); i++)
 	{
 		if (root[i][3] == 1)
 		{
 			continue;
 		}
-		subBox(&root[i][0], root[i].size(), exceptBox);
+		node *box;
+		box->where = &root[i][0];
+		dividBox(box, root[i].size(), exceptBox);
 	}
 
 	return 0;
